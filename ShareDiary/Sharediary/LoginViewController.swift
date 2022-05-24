7ffed603ceaede5af3988ceb4484761cf165b15e
2018008313 <rmsdnjs518@gmail.com>
2022-05-24 15:06:55 +0900

    feat: login alert

--- ShareDiary/Sharediary/LoginViewController.swift
+++ ShareDiary/Sharediary/LoginViewController.swift
@@ -26,7 +26,12 @@ class LoginViewController: UIViewController{
             }
             else{
                 print("login fail")
+                let LoginFailAlert = UIAlertController(title: "알림", message: "존재하지 않는 유저 정보입니다.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+                LoginFailAlert.addAction(ok)
+                self.present(LoginFailAlert, animated: true, completion: nil)
             }
+            //#TODO login fail reason
         }
     }
     
@@ -34,12 +39,23 @@ class LoginViewController: UIViewController{
         Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user,error) in
             if user != nil{
                 print("Signin Success")
+                let SigninSuccessAlert = UIAlertController(title: "알림", message: "회원가입이 완료되었습니다.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+                SigninSuccessAlert.addAction(ok)
+                self.present(SigninSuccessAlert, animated: true, completion: nil)
             }
             else{
                 print("Signin Fail")
+                let SigninFailAlert = UIAlertController(title: "알림", message: "회원가입에 실패했습니다.", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "확인", style: .default, handler: nil)
+                SigninFailAlert.addAction(ok)
+                self.present(SigninFailAlert, animated: true, completion: nil)
             }
+            //#TODO signin fail reason
         }
     }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
